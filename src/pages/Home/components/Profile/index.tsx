e42c88ed3be33470490ffe32f0ef5ba4e545dc99
felipesanderp <felipe.panisson@hotmail.com>
2023-08-24 15:02:49 -0300

    profile styles

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,5 +1,37 @@
-import { ProfileContainer } from './styles'
+import { FaBuilding, FaGithub, FaUserGroup } from 'react-icons/fa6'
+
+import { ProfileContainer, UserInfo, UserInfoIcons } from './styles'
 
 export function Profile() {
-  return <ProfileContainer></ProfileContainer>
+  return (
+    <ProfileContainer>
+      <img src="https://github.com/felipesanderp.png" alt="Felipe Sander" />
+
+      <UserInfo>
+        <h3>Felipe Sander</h3>
+        <span>
+          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
+          pulvinar vel mass.
+        </span>
+
+        <UserInfoIcons>
+          <span>
+            <FaGithub />
+            felipesanderp
+          </span>
+
+          <span>
+            <FaBuilding />
+            Rocketseat
+          </span>
+
+          <span>
+            <FaUserGroup />
+            32 seguidores
+          </span>
+        </UserInfoIcons>
+      </UserInfo>
+    </ProfileContainer>
+  )
 }
--- src/pages/Home/components/Profile/styles.ts
+++ src/pages/Home/components/Profile/styles.ts
@@ -4,8 +4,48 @@ export const ProfileContainer = styled.section`
   width: 100%;
   max-width: 864px;
   height: 13rem;
-
+  border-radius: 10px;
+  padding: 2.5rem 2rem;
   background: ${(props) => props.theme['base-profile']};
 
   margin-top: -8rem;
+
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+
+  > img {
+    width: 148px;
+    height: 148px;
+    border-radius: 8px;
+  }
+`
+
+export const UserInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+
+  h3 {
+    color: ${(props) => props.theme['base-title']};
+    font-size: 1.5rem;
+    font-weight: bold;
+    line-height: 1.5rem;
+  }
+
+  span {
+    line-height: 1.5rem;
+  }
+`
+export const UserInfoIcons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1.5rem;
+  margin-top: 1rem;
+
+  span {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
 `
